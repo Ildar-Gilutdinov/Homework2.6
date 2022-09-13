5385339b431dec22a1ff9b954c65c5d2fdf053bf
Ildar Gilmutdinov <ir.gilmutdinov@mail.ru>
2022-09-14 01:10:52 +0400

    first commit

--- src/Person.java
+++ src/Person.java
@@ -4,11 +4,12 @@ public class Person {
     public String surName;
     public int ticket;
 
-    public Person (String name, String surName, int ticket) { //Создание конструктора
+    public Person(String name, String surName, int ticket) { //Создание конструктора
         this.name = name;
         this.surName = surName;
         this.ticket = ticket;
     }
+
     public String toString() { // ввызов метода toString
         return name + " " + surName + " имеет кол-во биллетов " + ticket;
     }
